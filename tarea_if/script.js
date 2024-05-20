60dbf480163c9cc13abbf6396646c0a95881b693
walter.wow <walteroscarwagner@gmail.com>
2024-05-20 15:51:16 -0300

    ff

---
+++ tarea_if/script.js
@@ -0,0 +1,21 @@
+
+
+
+let edad = prompt('ingrese su edad')
+
+if (edad <= 0)
+    {
+        console.log('edad no valida')
+    }
+    else if ( edad <=13)
+    {
+        console.log('eres un niño')
+    }
+    else if ( edad = 13 <= 17)
+    {
+        console.log('eres un adolecente')
+    }
+    if ( edad >= 18 <= 64)
+    {
+        console.log('eres un adulto')
+    }
