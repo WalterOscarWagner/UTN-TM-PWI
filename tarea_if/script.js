810c211b26511867ada16ab6a4bcc478b5b02998
walter.wow <walteroscarwagner@gmail.com>
2024-05-21 14:42:16 -0300

    tarea

--- tarea_if/script.js
+++ tarea_if/script.js
@@ -1,5 +1,5 @@
 
-
+/*
 
 let edad = prompt('ingrese su edad')
 
@@ -7,15 +7,158 @@ if (edad <= 0)
     {
         console.log('edad no valida')
     }
-    else if ( edad <=13)
+    else if( edad < 13)
     {
         console.log('eres un niño')
     }
-    else if ( edad = 13 <= 17)
+    else if(edad >= 13 && edad <=17)
+        {
+            console.log('eres un adolescente')
+        }
+        else if(edad >= 18 && edad <= 64)
+        {
+            console.log('eres un adulto')
+        }
+        else if(edad >= 65)
+        {
+            console.log('eres una persona mayor')
+        }
+*/
+
+/*
+
+let calificacion = prompt('Ingrese calificacion numerica')
+
+if(calificacion >= 90 && calificacion <= 100)
     {
-        console.log('eres un adolecente')
+        console.log('A')
     }
-    if ( edad >= 18 <= 64)
+    else if(calificacion >= 80 && calificacion <= 89)
     {
-        console.log('eres un adulto')
+        console.log('B')
     }
+    else if(calificacion >= 70 && calificacion <= 79)
+    {
+        console.log('C')
+    }
+    else if(calificacion >= 60 && calificacion <= 69)
+    {
+        console.log('D')
+    }
+    else if(calificacion >= 0 && calificacion <=59)
+    {
+        console.log('F')
+    }
+    else if(calificacion > 100)
+    {
+        console.log('Calificacion no valida')
+    }
+    */
+
+
+
+/*
+let longitud_1= prompt('Ingresa primer longitud del triangulo')
+let longitud_2= prompt('Ingresa segunda longitud del triangulo')
+let longitud_3= prompt('Ingresa tercer longitud del triangulo')
+
+if(logitud_1 <= 0 && longitud_2 <= 0 && longitud_3 <= 0)
+    {
+        console.log('Longitudes no validas')
+    }
+    
+
+
+
+
+
+
+
+
+
+*/
+
+
+/*
+
+let numero = prompt ('ingresa un numero del 1 al 7')
+
+if( numero === '1')
+    {
+        console.log('LUNES')
+    }
+    else if(numero ==='2')
+    {
+        console.log('Martes')
+    }
+    else if(numero === '3')
+    {
+        console.log('Miercoles')
+    }
+    else if(numero === '4')
+    {
+        console.log('Jueves')
+    }
+    else if(numero === '5')
+    {
+        console.log('Viernes')
+    }
+    else if(numero === '6')
+    {
+        console.log('Sabado')
+    }
+    else if(numero === '7')
+    {
+        console.log('Domingo')
+    }
+    else{
+        console.log('Numero no valido')
+    }
+*/
+
+
+
+
+let grados= prompt('Grados Celsius')
+
+if(grados <= 0)
+    {
+        console.log('Solido')
+    }
+    else if(grados >= 1 && grados <= 99)
+    {
+        console.log('Liquido')
+    }
+    else if (grados >= 100)
+    {
+        console.log('Gaseoso')
+    }
+    else
+    {
+        console.log('Medida no valida')
+    }
+
+
+
+
+/*
+let lado1 = prompt('imgrese medida lado 1')
+let lado2 = prompt('ingrese medida lado 2')
+let lado3 = prompt('ingrese medida lado 3')
+
+if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0)
+    {
+        console.log('Medidas no validas')
+    }
+    else if(lado1 == lado2 & lado2 == lado3)
+    {
+        console.log('Equilatero')
+    }
+    else if(lado1 === lado2 || lado1 === lado3 || lado2 === lado3)
+    {
+        console.log('Isosceles')
+    }
+    else{
+        console.log('Escaleno')
+    }
+*/
