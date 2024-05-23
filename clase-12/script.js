f1e14cb26bc04ee60aa9c71369e5bf99ea34cfad
walter.wow <walteroscarwagner@gmail.com>
2024-05-23 15:02:06 -0300

    pd

--- clase-12/script.js
+++ clase-12/script.js
@@ -47,6 +47,8 @@ for(let i = 1; i <= 3; i = i +1){
     alert(nombre)
 }
 */
+
+/*
 let listaDeNumeros = 0
 let sumaNumeros =''
 
@@ -58,6 +60,101 @@ for(let i = 1 ; i <= 3 ; i = i + 1) {
 }
 alert(listaDeNumeros)
 alert(sumaNumeros)
+*/
+
+
+
+/*
+vamos a solicitar al usuario una cantidad de repeticiones
+vamos a solicitar un numero la cantidad de repeticiones ingresada por el usuario,al final mostrar el resultado de la suma
+validad que el numero ingresado sea un numero,en caso de no vollver a solicitarlo
+validar que la cantidad de repeticiones ingresada sea un numero, en caso que no volver a solicitarlo
+
+
+let cantidadDeRepeticiones = prompt("cuantos numeros sumamos?")
+let suma = 0
+while (!cantidadDeRepeticiones || isNaN(cantidadDeRepeticiones)) {
+    alert("Dato no valido")
+    cantidadDeRepeticiones = prompt("Ingrese un dato numerico")
+}
+for (let iteracion = 1; iteracion <= cantidadDeRepeticiones; iteracion = iteracion + 1) {
+    numero = prompt("Ingrese un numero")
+    while (!numero || isNaN(numero)) {
+        alert("Dato no valido")
+        numero = prompt("Ingrese un dato numerico")
+    }
+    suma = suma + Number(numero)
+}
+alert("El resultado es" + suma)
+
+*/
+
+/*
+// FUNCIONES
+
+//Declarar una funcion
+function saludar(nombre){
+    alert('hola a' + nombre)
+}
+
+
+//Invocar mi funcion
+saludar('pepe')
+saludar('juan')
+
+*/
+
+
+/*
+
+
+//Crear una funcion llamada sumar,que reciba dos numeros y muestre por consola el resultado de la suma
+
+function sumar(numero1, numero2){
+    let suma = numero1 +numero2
+    console.log(suma)
+    
+}
+
+sumar(5,6)
+
+*/
+/*
+
+Crear una funcion llamada calcularIva que reciba un precio y muestr el iva por consola de dicho precio
+
+valor * 0.21
+
+ej: calcularIva(100) => console: 21
+
+function calcularIva(importe){
+    console.log(importe*0.21)
+}
+
+calcularIva(100)
+
+*/
+
+
+function calcularIva(importe){
+    return importe * 0.21
+}
+
+let iva = calcularIva(100)
+
+alert(iva)
+console.log(iva)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
