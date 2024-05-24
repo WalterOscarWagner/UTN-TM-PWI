c226c893e6fa44d90ecd0ebbc95ae6892b421ce1
walter.wow <walteroscarwagner@gmail.com>
2024-05-24 11:27:37 -0300

    tarea12

--- tarea-clase12/script.js
+++ tarea-clase12/script.js
@@ -1,5 +1,78 @@
+
+/* 
+
 function decirNombre(nombre){
-    alert('Hola + nombre')
+    alert('Hola '+ nombre)
+    
 }
 
 decirNombre('pepe')
+
+*/
+
+/*
+function saludar(nombre){
+    return String('hola' + nombre)
+}
+
+alert(saludar('juan'))
+
+*/
+
+
+/*
+function sumar(a,b){
+    return Number(a + b)
+}
+
+alert(sumar(1,8))
+
+*/
+
+/*
+function restar(a,b){
+    return Number(a-b)
+
+}
+
+alert(restar (8,5))
+
+*/
+
+
+function calcular(operacion,a,b){
+    if(operacion === '+'){
+        return Number(a + b)
+    }
+    else if(operacion === '-')
+        return Number(a-b)
+    else{
+        return null
+    }
+
+}
+
+
+alert(calcular('-',15,9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
