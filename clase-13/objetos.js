4ae3ebadbb7d0f328cf42c99b442c64761cf9cbe
walter.wow <walteroscarwagner@gmail.com>
2024-05-27 16:11:05 -0300

    clase13

--- clase-13/objetos.js
+++ clase-13/objetos.js
@@ -47,41 +47,149 @@ let producto ={
 console.log('el producto' + producto['nombre_completo'] + 'cuesta $' + producto['precioUnitario'])
 
 */
-
-
-const user = {
-    name:'pepe',
-    lastname: 'suarez',
-    'user preferences':{
-        theme: 'dark-mode',
-        lang: 'spanish'
-    }
-}
-
+/* 
+'El usuario {name} {lastname} creo su cuenta en fecha {created_at} vive en {adress} y su telefono es {tel}'
+*/
 /*
 Por consola mostrar un informe del usuario
 'El usuario{nombre} {lastname} tiene las preferencias
-de usuario en : {theme}\nLenguaje: {lang}'*/
+de usuario en : {theme}\nLenguaje: {lang}'
+
 
 console.log('El usuario :' + user['name'] +'' + user['lastname']
- + 'tiene las preferencias en :' + '\n' + user['user preferences']['theme']
-  + '\n' + user['user preferences']['lang'])
++ 'tiene las preferencias en :' + '\n' + user['user preferences']['theme']+ '\n' + user['user preferences']['lang'])
+
+*/
+/*
+const user = {
+    name:' pepe ',
+    lastname: ' suarez ',
+    'user preferences':{
+        theme: ' dark-mode ',
+        lang: ' spanish '
+    },
+    'user info':{
+        'created_at': ' 18/9/2005 ',
+        'adress': ' av siempre viva 742 ',
+        tel: ' 22 1323-2122 '
+    }
+}
+*/
+/* reasignamos el valor de la propiedad name
+
+user['name']  = 'juan'
+user.lastname = 'gomez'
+*/
+/* Creando una propiedad 
+
+user['isAdmin'] = false
+user.isAdmin = true
+*/
+
+//NOusamos notacion de . si la prpiedad tiene -,ñ,letras con tilde o espacios
+
+
+/*
+console.log('El usuario :' + user['name'] +'' + user['lastname'] +' creo su cuenta en fecha'
++ user['user info']['created_at'] + 'vive en' + user['user info']['adress'] + 'y su telefono es'
++ user['user info']['tel'])
+*/
+
+
+
+// TEMPLADE STREAM
+/*
+console.log(`
+    El usuario ${user['name']} ${user['lastname']} tiene las preferencias en:
+    -Modo: ${user['user preferences']['theme']}
+    -Lenguaje: ${user['user preferences']['lang']}
+`)
+*/
+
+/* PASAR LOS MENSAJES DE CONSOLA A TEMPLATE STRING*/
+
+/*
+console.log(`
+El usuario ${user['name']} ${user['lastname']} creo su cuenta en fecha
+${user ['user info']['created_at']}
+vive en ${user['user info'] ['adress']}
+y su telefono es ${user['user info']['tel']}
+`)
+*/
+//Esto se puede abreviar usando .
+
+/*
+console.log(`
+    El usuario ${user['name']} ${user['lastname']} tiene las preferencias en:
+    -Modo: ${user['user preferences']['theme']}
+    -Lenguaje: ${user['user preferences']['lang']}
+`)
+
+
+console.log(`
+El usuario ${user.name} ${user.lastname} tiene las preferencias en:
+-Modo: ${user['user preferences'].theme}
+-Lenguaje: ${user['user preferences'].lang}
+`)
+
+*/
+
+
+/* Crear una funcion llamada calcularIva(precio,tipo_factura)
+tipo_factura 'A,'B','C'
+
+
+En el caso de so la factura es C o B el precio tendra el iva incluido
+Ejemplo
+calcularIva(100, 'C'|'B')
+{ 
+    precio: 121
+    iva: 21
+    precioConIva: 121
+    factura: 'B' | 'C'
+}
+
+En el caso de si la factura ea A el precio tendra el iva discriminado
+Ejemplo
+calcularIva(100, 'A')
+{
+    precio: 100
+    iva: 21
+    precioConIva: 121
+    factura: 'A'
+}
+{
+    precio: number
+    iva: number
+    precioConIva,
+    factura
+}
+
+*/
+
+
+const calcularIva = (precio, tipo_factura ) => {
+    let iva = precio * 0.21
+
+    const resultado = {
+        precio: null,
+        iva: iva,
+        precioConIva: precio + iva,
+        factura: tipo_factura
+    }
+    if (tipo_factura == 'C' || tipo_factura == 'B'){
+        resultado.precio = precio + iva
+    }
+    else if (tipo_factura == 'A') {
+        resultado.precio = precio
+    }
+    return resultado
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(calcularIva(1000,'A'))
 
 
 
