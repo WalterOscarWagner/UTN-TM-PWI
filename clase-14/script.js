c93fd341aff75a5b288f5350310e1b88860944a6
walter.wow <walteroscarwagner@gmail.com>
2024-05-28 11:27:23 -0300

    clase-14

---
+++ clase-14/script.js
@@ -0,0 +1,24 @@
+// arrays
+// lista de elementos ordenados
+
+let nombre1 = 'pepe'
+let nombre2 = 'juan'
+let nombre3 = 'ezequiel'
+
+
+
+//posicion/index
+const nombres = ['pepe,', 'juan', 'ezequiel']
+
+
+console.log(nombres[1])
+console.log('Tengo' + nombres.length + 'nombres')
+
+
+//metodos de array
+
+//.push(): agerga un elemento al final del array y devuelve la nueva longitud
+
+
+nombres.push('maria')
+console.log(nombres)
