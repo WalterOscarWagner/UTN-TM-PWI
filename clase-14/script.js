42d8636b3fb1c9532b7e78f9af639777ebc629bf
walter.wow <walteroscarwagner@gmail.com>
2024-05-31 14:15:27 -0300

    clase-14

--- clase-14/script.js
+++ clase-14/script.js
@@ -1,24 +1,165 @@
 // arrays
-// lista de elementos ordenados
 
-let nombre1 = 'pepe'
-let nombre2 = 'juan'
-let nombre3 = 'ezequiel'
+/*
 
-
-
-//posicion/index
-const nombres = ['pepe,', 'juan', 'ezequiel']
+const nombres3 = [ 'maria' , 'pedro' , 'leonel' , 'ezequiel' , 'elias' , 'lucas' , 'julieta' , 'abril' , 'lautaro']
+*/
 
 
-console.log(nombres[1])
-console.log('Tengo' + nombres.length + 'nombres')
+
+//Eliminar a leonel
+//eliminar al anterior a lucas
+//agregar antes de abril a 'sofia' sin eliminar a abril
 
 
-//metodos de array
+/*
 
-//.push(): agerga un elemento al final del array y devuelve la nueva longitud
+let leonelPosition = nombres3.indexOf('leonel')
+nombres3.splice(leonelPosition,1)
+console.log(nombres3)
 
 
-nombres.push('maria')
-console.log(nombres)
+let lucasPosition = nombres3.indexOf('lucas')
+nombres3.splice((lucasPosition -1),1)
+console.log(nombres3)
+
+
+let abrilPosition = nombres3.indexOf('abril')
+nombres3.splice(abrilPosition,0 , 'sofia')
+console.log(nombres3)
+
+*/
+/*
+for(let index = 0; index < nombres3.length; index =index + 1){
+    console.log(nombres3[index])
+}
+*/
+
+/*
+const nombres3 = [ 'maria' , 'pedro' , 'leonel' , 'ezequiel' , 'elias' , 'lucas'
+, 'julieta' , 'abril' , 'lautaro']
+
+*/
+
+/*
+Obtener cantidad total de caracteres de una lista de nombres:
+Quiero que dado el array de nombre, por cada nombre sumes la cantidad de caracteres 
+Al finalizar el recorrido decir ' la cantidad de caracteres es:{sumatoria de caracteres}'
+
+
+Ejemplo: ['pepe','juan'] // = 8
+*/
+
+
+/*
+let caracteres = 0
+for (let index = 0 ; index < nombres3.length ; index = index + 1 ){
+    caracteres = caracteres + nombres3[index].length
+}
+
+console.log( 'la cantidad de caracteres es: ' +  caracteres )
+*/
+
+
+// Dado el siguiente array crear un segundo array llamado mayusculas que solo contenga alguna palabra con alguna letra mayuscula 
+
+
+
+/*
+const nombres =['pepe', 'lucas', 'PEDRO', 'JuaN']
+
+//esto debe generar un segundo array con ['PEDRO', 'JuaN']
+
+const estaEnMinuscula = (string) => {
+    return string == string.toLowerCase()
+} 
+
+const tieneMayuscula = (string) => {
+    return !estaEnMinuscula(string)
+}
+
+
+const mayusculas =[];
+for (let nombre of nombres){
+    if (tieneMayuscula(nombre)){
+        mayusculas.push(nombre);
+    }
+}
+
+console.log(mayusculas);
+
+*/
+
+
+const productos = [
+    {
+        title: 'tv samsung 32"',
+        price: 3000,
+        marca: 'samsung'
+    },
+    {
+        title: 'tv samsung 50"',
+        price: 4600,
+        marca: 'samsung'
+    },
+    {
+        title: 'tv lg 42"',
+        price: 3500,
+        marca: 'lg'
+    },
+    {
+        title: 'tv noblex 50"',
+        price: 4500,
+        marca: 'noblex'
+    },
+    {
+        title: 'tv noblex 65"',
+        price: 6000,
+        marca: 'noblex'
+    },
+
+]
+
+/* crear un array llamado samsung que contenga los productos que sean de la marca samsung */
+
+
+
+const samsung = []
+for(const producto of productos){
+    if (producto.marca === 'samsung'){
+        samsung.push(producto)
+    }
+}
+console.log(samsung)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
