f637bced6e01684f454eb5fe7e26d9b3d8a50681
walter.wow <walteroscarwagner@gmail.com>
2024-06-06 09:59:24 -0300

    calss15

--- clase-15/callculadoraMejorada.js
+++ clase-15/callculadoraMejorada.js
@@ -1,4 +1,31 @@
+/*
 
+## login
+
+Debo tener una funcion llamada login que active toda la logica del login
+
+logica del login:
+
+Vamos a solicitar al usuario un email
+Validaremos que el email sea un email
+/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(stringAValidar)
+
+Ejemplo de uso
+/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test('pepe@gmail.com)
+
+
+Vamos a solicitar al usuario una password
+validar que tenga mas de 6 caracteres, que tenga al menos 1 mayuscula y que no sea '' o null
+
+Si todo esta correcto dira por alerta('usuario{email}registrado con exito) y retornara al usuario como objeto:
+
+EJEMPLO:
+
+login()  //retorna el usuario {
+    email: 'value@gmail.com' ,
+    password: 'Pepe123'
+}
+*/
 
 const tieneMayuscula = (palabra) => palabra.toLowerCase() !== palabra
 
