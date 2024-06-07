c722e6a7188ca37369fcac1ad50dd73e009a0418
walter.wow <walteroscarwagner@gmail.com>
2024-06-07 14:52:22 -0300

    clase14

--- clase-15/calculadora.js
+++ clase-15/calculadora.js
@@ -1,40 +1,39 @@
 /*
 ## login
 
-Debo tener una funcion llamada login que active toda la logica del login
+debo tener una funcion llamada login que active toda la logica del login
 
 logica del login:
 
-Vamos a solicitar al usuario un email
-Validaremos que el email sea un email
-/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(stringAValidar)
-
-Ejemplo de uso
+vamos a solicitar al usuario un email
+validaremos que el email sea un email
 /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test('pepe@gmail.com)
 
 
-Vamos a solicitar al usuario una password
-validar que tenga mas de 6 caracteres, que tenga al menos 1 mayuscula y que no sea '' o null
+vamos a solicitar al usuario una password
+validar que tenga igual o mas de 6 carcteres,que tenga al menos 1 mayuscula y que no sea '' o null
 
-Si todo esta correcto dira por alerta('usuario{email}registrado con exito) y retornara al usuario como objeto:
+si todo esta correcto dira por alerta('Usuario{email} registrado con exito') y retornara al usuario como objeto:
 
 EJEMPLO:
 
-login()  //retorna el usuario {
-    email: 'value@gmail.com' ,
+login() // retorna el usuario {
+    email: 'value@gmail.com',
     password: 'Pepe123'
 }
-*/
+    */
 
 
-/*
 const tieneMayuscula = (palabra) => palabra.toLowerCase() !== palabra
-const validacionEmail = (email) =>{
+
+const validacionEmail = (email) => {
     return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)
 }
+
 const validacionPassword = (password) =>{
     return password && password.length >= 6 && tieneMayuscula(password)
 }
+
 
 
 const login = () => {
@@ -43,15 +42,15 @@ const login = () => {
         password: null
     }
 
-    let email = prompt('ingrese un email')
+    let email = prompt('Ingrese un email')
     while(!validacionEmail(email)){
-        email = prompt('error email invalido')
+        email = prompt('Error email invalido: ingrese nuevamente')
     }
     usuario.email = email
 
-    let password = prompt('ingrese el password')
+    let password = prompt('Ingrese la password que tenga mas de 6 caracteres y una mayuscula')
     while(!validacionPassword(password)){
-        password = prompt('error password invalido')
+        password = prompt('Error password invalida, vuelva a ingresar una password de 6 caracteres y una mayuscula')
     }
     usuario.password = password
 
@@ -59,72 +58,21 @@ const login = () => {
 
     return usuario
 }
-*/
-/*
+
+
 console.log(login())
-*/
-
-/*
-## Calculadora de operaciones
-
--Operaciones disponibles:
-    1) +
-    2) -
-
-Solicitar una operacion y verificar que la operacion sea valida
-(es valida si esta dentro de las operaciones disponibles)
-Solicitar un numero 1 y validarlo
-Solicitar un numero 2 y validarlo
-
-Al finalizar las solicitudes mostrar el resultado por alerta de la
-sig manera: ' El resultado de {numero1} {operacion} {numero2} es {resultado}'
-
-*/
 
 
-const validacionOperacion = (operacion) =>{
-    const OPERACIONES_DISPONIBLES = ['+', '-']
-    for(let operacionDisponible of OPERACIONES_DISPONIBLES){
-        if(operacionDisponible == operacion){
-            return true
-        }
-    }
-    return false
-}
 
 
-const validacionNumero = (numero) =>{
-    return numero && !isNaN(numero)
-}
 
 
-const calculadora = () =>{
-    let operacion = prompt('Ingrese una operacion')
-    while(!validacionOperacion(operacion)){
-        operacion = prompt('Error operacion no disponible,seleccione una operacion')
-    }
 
-    let a =prompt('Ingrese un primer numero')
-    while(!validacionNumero(a)){
-        a = prompt('Error numero no valido, ingrese nuevamente')
-    }
 
-    let b = prompt('Ingrese un numero')
-    while(!validacionNumero(b)){
-        b = prompt('Error numero no valido, ingrese nuevamente')
-    }
 
-    let resultado = 0
 
-    if(operacion === '+'){
-        resultado = Number(a)+ Number(b)
-    }
-    else if(operacion === '-'){
-        resultado = Number(a) - Number(b)
-    }
 
-    alert(`El resultado de ${a} ${operacion} ${b} es ${resultado}`)
-}
+
 
 
 
--- clase-15/documentacion.md
+++ clase-15/documentacion.md
@@ -1,73 +1,65 @@
+### calciladora
 
-### Calculadora
+## funcionalidades:
 
-## Funcionalidades:
+-login
+    -solicitar un email
+    -solicitar un password
 
--Login
--Calcular Operaciones
-    -Operaciones Disponibles:
+
+-calcular operaciones
+    -operaciones disponibles
     1) +
-    2) 
--Historial
+    2) -
+
+
+-historial
     -agregarAlHistorial
     -mostrarHistorial/renderizarHistorial
 
-## Utilidades:
+## utilidades
 
--Validaciones
+- validaciones
     -numero
     -password
     -email
     -operacionesCalculadora
 
-## Reglas
+## reglas
 
-Los ejercicios se pueden resolver de muchas formas
-Si crees necesario usar funciones, las podes usar
-Cumplir en la medida de lo posible con el principio: DRY, KISS
+los ejercicion se pueden resolver de muchas formas
+si crees necesario usar funciones las podes usar
+cumplir en la medida de lo posible con el principio DRY, KISS
 
 ## RECOMENDACIONES:
 
+Una funcion la usamos para evitar re-escribir logica o separar logica
+
 ## login
 
-Debo tener una funcion llamada login que active toda la logica del login
+debo tener una funcion llamada login que active toda la logica del login
 
 logica del login:
 
-Vamos a solicitar al usuario un email
-Validaremos que el email sea un email
+vamos a solicitar al usuario un email
+validaremos que el email sea un email
 /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(stringAValidar)
 
-Ejemplo de uso
-/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test('pepe@gmail.com)
+vamos a solicitar al usuario una password
+validar que tenga igual o mas de 6 carcteres,que tenga al menos 1 mayuscula y que no sea '' o null
 
-
-Vamos a solicitar al usuario una password
-validar que tenga mas de 6 caracteres, que tenga al menos 1 mayuscula y que no sea '' o null
-
-Si todo esta correcto dira por alerta('usuario{email}registrado con exito) y retornara al usuario como objeto:
+si todo esta correcto dira por alerta('Usuario{email} registrado con exito') y retornara al usuario como objeto:
 
 EJEMPLO:
 
-login()  //retorna el usuario {
-    email: 'value@gmail.com' ,
+login() // retorna el usuario {
+    email: 'value@gmail.com',
     password: 'Pepe123'
 }
 
 
 
 
-## Calculadora de operaciones
-
--Operaciones disponibles:
-    1) +
-    2) -
-
-Solicitar una operacion y verificar que la operacion sea valida (es valida si esta dentro de las operaciones disponibles)
-Solicitar un numero 1 y validarlo
-Solicitar un numero 2 y validarlo
-
-Al finalizar las solicitudes mostrar el resultado por alerta de la sig manera: ' Elresultado de {numero1} {operacion} {numero2} es {resultado}'
 
 
 
