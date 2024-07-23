3a4ec4a50f43f826856c42a5dc0ec7059e47fce1
walter.wow <walteroscarwagner@gmail.com>
2024-07-23 15:28:09 -0300

    dddd

--- clase-11/practica.js
+++ clase-11/practica.js
@@ -55,11 +55,73 @@ else{
     console.log('acceso denegado')
 }
 
+/*
+1)
+Escribe un programa que le pida al usuario que introduzca su edad mediante un prompt.
+Luego, el programa debe determinar y mostrar en la consola en qué etapa de la vida 
+se encuentra el usuario según su edad. Las etapas son las siguientes:
+Si la edad es menor de 0, mostrar "Edad no válida."
+Si la edad es menor de 13, mostrar "Eres un niño."
+Si la edad es entre 13 y 17 inclusive, mostrar "Eres un adolescente."
+Si la edad es entre 18 y 64 inclusive, mostrar "Eres un adulto."
+Si la edad es 65 o mayor, mostrar "Eres una persona mayor."
+*/
+
+/*
+2)
+Crea un programa que le pida al usuario que introduzca una calificación numérica
+(entre 0 y 100) mediante un prompt. El programa debe convertir la calificación a una letra según
+la siguiente escala:
+90 a 100: A
+80 a 89: B
+70 a 79: C
+60 a 69: D
+0 a 59: F
+Si la calificación introducida está fuera del rango de 0 a 100, el programa debe mostrar
+"Calificación no válida."
+*/
+
+/*
+3)
+Desarrolla un programa que le pida al usuario las longitudes de los tres lados
+de un triángulo mediante tres prompts separados. El programa debe determinar y mostrar
+en la consola el tipo de triángulo que se forma (equilátero, isósceles o escaleno) según
+las longitudes de los lados introducidos:
+Equilátero: todos los lados son iguales.
+Isósceles: dos lados son iguales.
+Escaleno: todos los lados son diferentes.
+Si alguna de las longitudes introducidas es menor o igual a 0, el programa debe mostrar
+"Longitudes no válidas."
+
+*/
+
+/*
+4)
+Escribe un programa que le pida al usuario que introduzca un número del 1 al 7 mediante un prompt.
+El programa debe mostrar en la consola el día de la semana correspondiente al número introducido:
+1: Lunes
+2: Martes
+3: Miércoles
+4: Jueves
+5: Viernes
+6: Sábado
+7: Domingo
+Si el número introducido no está en el rango de 1 a 7, el programa debe mostrar "Número no válido."
+
+*/
 
 
+/*
+5)
+Crea un programa que le pida al usuario que introduzca una temperatura en grados Celsius mediante
+un prompt. El programa debe determinar y mostrar en la consola el estado del agua a esa temperatura:
+Sólido: 0 grados Celsius o menos.
+Líquido: entre 0 y 99 grados Celsius.
+Gaseoso: 100 grados Celsius o más.
+Si la temperatura introducida no es un número válido, el programa debe mostrar 
+"Temperatura no válida."
 
-
-
+*/
 
 
 
