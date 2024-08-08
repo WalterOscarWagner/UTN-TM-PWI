42f2611a6a48dfdd09d44302ef06a6bbfd2759e8
walter.wow <walteroscarwagner@gmail.com>
2024-08-08 13:58:42 -0300

    ert

--- clase-15/localStorage.js
+++ clase-15/localStorage.js
@@ -1,17 +1,44 @@
 // localStorage y sessionStorage
 
-
+/*
 let nombre = prompt ('Ingrese un nombre')
 
 localStorage.setItem('username', nombre)
 
-
+localStorage.clear()
+*/
 /*
+
 console.log(String({nombre: 'pepe'}))
+
+*/
+/*
+//JSON
+
+// String escrito en formato JSON
+
+'{"nombre": "pepe"]'
+
+// .parse permite transformar un string con notacion de objetos a un objeto
+
+console.log(JSON.parse('{"nombre": "pepe"}'))
+
+
+// stringify
+
+const producto = {
+    nombre: 'tv samsung',
+    precio: 700
+}
+let stringConNotacionDeObjeto = JSON.stringify(producto)
+
+console.log(stringConNotacionDeObjeto)
+
+
 */
 
-
 /*
+
 const user = {nombre: 'pepe'}
 
 localStorage.setItem('user', JSON.stringify(user))
@@ -20,18 +47,11 @@ const usuarioFromLocal = JSON.parse(localStorage.getItem('user'))
 
 console.log(usuarioFromLocal.nombre)
 
-
-
-'{"nombre": "pepe"}'
-
-
-
-console.log(JSON.parse('{"nombre": "pepe"}'))
-
 */
 
 
-/*
+
+
 
 const login = () => {
     const usuario = {
@@ -56,19 +76,35 @@ const login = () => {
     return usuario
 }
 
-*/
+console.log(login(usuario))
+
+
 //Mejorar la funcion login para que el objeto retornado lo guarde en localStorage
-
 /*
+const login = () => {
+    const usuario = {
+        email: null,
+        password: null
+    }
+    usuario.email = email
+    usuario.password = password
+    alert(`usuario ${usuario.email} registrado`)
 
+    return usuario
+}
+*/
+/*
 let usuarioRegistrado = JSON.stringify(login())
 localStorage.setItem('usuario', usuarioRegistrado)
-*/
-/*
 
+console.log(usuarioRegistrado)
+
+*/
+
+
+/*
 console.log(JSON.parse(localStorage.getItem('usuario')))
 */
-
 /*
 
 localStorage.setItem( 
